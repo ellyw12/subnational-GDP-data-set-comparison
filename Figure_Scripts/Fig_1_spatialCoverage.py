de543e7b93c06390cd952f92ac031bdc9aae1b3e
ellyw12 <brielle.elise.wells@student.hu-berlin.de>
2025-05-21 10:20:14 +0200

    fix legend

--- Figure_Scripts/Fig_1_spatialCoverage.py
+++ Figure_Scripts/Fig_1_spatialCoverage.py
@@ -148,21 +148,22 @@ for i, (col, ax, color, title) in enumerate(zip(data_columns[:2], axes[:2], pane
     ax.axis('off')
 
     
-    legend_patches = [
-        # mpatches.Patch(color=color_total, label=f"{col} (Data Available)"),
-    ]
-    if col == 'grp_lcu':  # Add a legend entry for narrower regions in the Kummu plot
-        legend_patches.append(
-            mpatches.Patch(label="DOSE lcu data available", color=color_total)
-        )
-    if col == 'K2025_pc':  # Add a legend entry for narrower regions in the Kummu plot
-        legend_patches.append(
-            mpatches.Patch(color=color_total, label=f"K2025)"),
-            mpatches.Patch(color=lighter_orange, label="K2025 Narrower than GID_1")
-        )
-        mpatches.Patch(color='lightgrey', label="No Data"), 
+# Define legends for Panel A and Panel B
+    legend_patches = []
+    if col == 'grp_lcu':  # Panel A: DOSE
+        legend_patches = [
+            mpatches.Patch(color=color_total, label="DOSE lcu data available"),
+            mpatches.Patch(color='lightgrey', label="No Data")
+        ]
+    elif col == 'K2025_pc':  # Panel B: Kummu
+        legend_patches = [
+            mpatches.Patch(color=color_total, label="K2025"),
+            mpatches.Patch(color=lighter_orange, label="K2025 Narrower than GID_1"),
+            mpatches.Patch(color='lightgrey', label="No Data")
+        ]
+    
+    # Add the legend to the axis
     ax.legend(handles=legend_patches, loc='lower left', fontsize=12, frameon=True, handlelength=1.2, borderpad=0.5, labelspacing=0.4)
-
 
 # Create the third plot for global datasets
 global_ax = axes[2]
